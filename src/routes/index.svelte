c1633f39fa4d9f324b12f3510f1b8e78bdfcd226
Alex Tideman <alex.tideman@gmail.com>
2021-09-03 21:27:32 -0500

    Reorder

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,27 @@
-<script context="module" lang="ts">
-	export const prerender = true;
-</script>
-
 <script lang="ts">
-	import IndexCardRoller from '$lib/IndexCardRoller.svelte';
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
 </script>
 
-<svelte:head>
-	<title>Draft</title>
-</svelte:head>
+<Header />
 
-<IndexCardRoller slot="draft"/>
+<main>
+	<slot />
+</main>
 
+<!-- <footer>
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer> -->
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		/* max-width: 1024px; */
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
