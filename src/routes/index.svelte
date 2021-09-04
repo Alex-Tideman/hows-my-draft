1651b69d80ca288a417e3f0df33093596709ebb1
Alex Tideman <alex.tideman@gmail.com>
2021-09-03 21:36:43 -0500

    Fix layout isssue

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,24 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = true;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+</script>
 <script lang="ts">
-	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
+	import Analysis from '$lib/Analysis.svelte';
 </script>
 
-<Header />
+<svelte:head>
+	<title>Analysis</title>
+</svelte:head>
 
-<main>
-	<slot />
-</main>
-
-<!-- <footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer> -->
-
-<style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		/* max-width: 1024px; */
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-</style>
+<Analysis slot="analyze" />
