d4eeecbb85b06b06d281027048eef23a28afcd45
Alex Tideman <alex.tideman@gmail.com>
2021-09-14 11:56:57 -0500

    Routes

---
+++ src/routes/auth/callback.ts
@@ -0,0 +1,48 @@
+import { serialize } from "cookie";
+
+const YahooFantasy = require("yahoo-fantasy");
+const request = require("request");
+
+export default (req, res) => {
+  const yf = new YahooFantasy(
+    process.env.YAHOO_CLIENT_ID,
+    process.env.YAHOO_CLIENT_SECRET,
+    null,
+    `https://${process.env.APP_URL}/auth/callback`
+  );
+
+  yf.authCallback(req, (err, { access_token, refresh_token }) => {
+    if (err) {
+      return res.status(400).json({ err });
+    }
+
+    const options = {
+      url: "https://api.login.yahoo.com/openid/v1/userinfo",
+      method: "get",
+      json: true,
+      auth: {
+        bearer: access_token,
+      },
+    };
+
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        const user = {
+          id: body.sub,
+          name: body.nickname,
+          avatar: body.profile_images.image64,
+        };
+
+        res.setHeader("Set-Cookie", [
+          serialize("accessToken", access_token, { path: "/", httpOnly: true }),
+          serialize("refreshToken", refresh_token, {
+            path: "/",
+            httpOnly: true,
+          }),
+        ]);
+
+        return res.json({ user: user });
+      }
+    });
+  });
+};
